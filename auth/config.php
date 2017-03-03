4d49320f4038e2b66da1552b4b6aa17d125991d8
Ediblewildfungi <jianghhui@gmail.com>
2017-03-03 14:09:50 +0800

    update

---
+++ auth/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//数据库配置全局常量
+define("DBHOST","localhost");
+define("DBUSER","root");
+define("DBPWD","");
+define("DBNAME","cvsmp");
+
+ ?>
--- request/getpost.php
+++ request/getpost.php
@@ -1,10 +1,18 @@
 <?php
 
 include '../auth/auth.php';
-
+require_once("../auth/config.php");
 // 拼接回传代码
-$community = $_POST["community"].$_POST["community1"].$_POST["community2"].$_POST["community3"];
 
+function link_database(){
+  $link_id=mysql_connect(DBHOST,DBUSER,DBPWD);
+  mysql_select_db(DBNAME);
+
+
+
+}
+
+$community = $_POST["community"].$_POST["community1"].$_POST["community2"].$_POST["community3"];
 $str = $community;
 $key = "y1ubASctsW8CsOkmYALaW";
 $act = "ENCODE";
@@ -17,5 +25,27 @@ $community_add = array(
 );
 // json格式输出
 echo json_encode($community_add);
+echo "\n";
+$hash = password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
+echo $hash."\n";
+
+$gethash = '$2y$10$FQVo2BzZ29ejbbvCjK6qNexmDKnZQD.ty2ey8h30Kmip8jctF.0Zu';
+
+if (password_verify('rasmuslerdorf', $gethash)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.';
+}
+
 
  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <img src="http://qr.liantu.com/api.php?bg=ffffff&fg=92b37b&gc=1751a9&el=l&w=200&m=10&text=<?php echo $code;  ?>"/>
+  </body>
+</html>
